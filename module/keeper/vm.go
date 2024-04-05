a2a738422ae5f66152c54a95ae65f3bdcdee4491
SeUkKim <eleccookie@gmail.com>
2024-04-05 16:44:05 +0900

    remove code for testing.

--- module/keeper/vm.go
+++ module/keeper/vm.go
@@ -1,10 +1,4 @@
 package keeper
 
-import (
-	movekeeper "github.com/initia-labs/initia/x/move/keeper"
-)
-
 // VMKEeper is just a stub
-type VMKeeper struct {
-	movekeeper.Keeper
-}
+type VMKeeper struct{}
